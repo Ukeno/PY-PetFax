3dcc1073915715607a71f3f91051bc8a1ece3c72
Kenny K <koablood@gmail.com>
2023-03-29 00:10:36 -0700

    Database Integration-Flask

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,9 +1,18 @@
 # config                    
 from flask import Flask
+from flask_migrate import Migrate
 
 # factory
 def create_app():
     app = Flask(__name__)
+    
+    # database config
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:2J1bzy9489@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False     
+
+    from . import models 
+    models.db.init_app(app) 
+    migrate = Migrate(app, models.db)
 
     # index route
     @app.route('/')
